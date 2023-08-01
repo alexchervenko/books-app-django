50a2a1c6ebda21ce42d18326e191511533ff0d15
Alexey Chervenko <alexbusymann@gmail.com>
2023-08-01 17:12:15 +0300

    add models

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    name = "world"
+    return render(request, "base.html", {"name": name})
+
+def search(request):
+    search = request.GET.get('search')
+    return render(request, "search.html", {"search": search})
